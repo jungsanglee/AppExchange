5abe34d5139343560184f428e9124296b7293fac
jungsanglee <115109867+jungsanglee@users.noreply.github.com>
2022-10-05 21:59:38 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="theme-color" content="#000000" />
+  <meta name="description" content="Web site created using create-react-app" />
+  <link rel="apple-touch-icon" href="/logo192.png" />
+  <link rel="manifest" href="/manifest.json" />
+  <title>AppExchange</title>
+  <script defer="defer" src="/static/js/main.25d88db8.js"></script>
+  <link href="/static/css/main.224b02db.css" rel="stylesheet">
+</head>
+
+<body><noscript>Вам необходимо включить JavaScript, чтобы запустить это приложение.</noscript>
+  <div id="root"></div>
+</body>
+
+</html>
